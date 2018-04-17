e964b01cff2f9a759cc824300337dca292a5e040
szczesnym <marcins@oerlemans-foods.nl>
2018-04-17 23:46:41 +0200

    Ex. 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,14 @@
 package com.kodilla.testing;
 
+import java.lang.*;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    System.out.println("6. Wprowadzenie do testowania");
+    public static void main(String[] args) {
+        System.out.println("6. Wprowadzenie do testowania");
+        Calculator calculator = new Calculator();
+        System.out.format("SUM %f", calculator.addNumbers(5, 4));
+        System.out.format("SUM %f\n", calculator.addNumbers(5, 4));
+        System.out.format("DIVISION %f\n", calculator.divideNumbers(5, 4));
+    }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+import java.lang.*;
+import java.io.*;
+
+public class Calculator {
+    public float addNumbers(float x, float y) {
+        return x + y;
+    }
+    public float divideNumbers(float x, float y) {
+        return x - y;
+    }
+}
