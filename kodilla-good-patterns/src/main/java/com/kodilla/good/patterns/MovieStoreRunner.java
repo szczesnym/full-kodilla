2b3b7c68babc985556d52af94428b1900ef84b50
szczesnym <marcins@oerlemans-foods.nl>
2018-05-08 23:28:57 +0200

    Ex 9.1 v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/MovieStoreRunner.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/MovieStoreRunner.java
@@ -7,17 +7,12 @@ import java.util.stream.Collectors;
 public class MovieStoreRunner {
     public static void main(String[] args) {
         Map<String, List<String>> moviesWithTranslations = MovieStore.getMovies();
-//version 1
+
         String result = moviesWithTranslations.entrySet()
                 .stream()
                 .map(title -> title.getKey() + "!" + title.getValue().stream().collect(Collectors.joining("!")))
                 .collect(Collectors.joining("!"));
         System.out.println(result);
-//version 2
-        System.out.println();
-        moviesWithTranslations.entrySet()
-                .stream()
-                .map(title -> title.getKey() + "!" + title.getValue().stream().collect(Collectors.joining("!")))
-                .forEach(System.out::println);
+
     }
 }
