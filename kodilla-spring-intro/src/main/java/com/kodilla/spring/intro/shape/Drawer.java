6b177b92852a93c716064e44acaa4a60fe90b808
szczesnym <marcins@oerlemans-foods.nl>
2018-05-17 14:27:01 +0200

    Ex. 10.1 Bean ForumUser

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -0,0 +1,13 @@
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+    public void doDrawing() {
+        Figure figure;
+
+        figure = new Circle();
+        figure.draw();
+
+        figure = new Triangle();
+        figure.draw();
+    }
+}
