d9ae13f946039fe3bb923b96aec689f3cec76462
Marcin Szczesny <marcins@oerlemans-foods.nl>
2018-05-29 16:16:44 +0200

    EX 13.1 v1

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DbManager {
-    private Connection conn;
     private static DbManager dbManagerInstance;
+    private Connection conn;
 
     private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
